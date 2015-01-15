e2fa21e07ed167cceb35a328bbce9d915bb10ac2
Apostle-z01 <haritabh.z01@gmail.com>
2015-01-16 03:41:11 +0530

    Map Integrated.

---
+++ app/src/main/java/com/example/prithwishmukherjee/duvidha/Globals.java
@@ -0,0 +1,10 @@
+package com.example.prithwishmukherjee.duvidha;
+
+import android.widget.EditText;
+
+/**
+ * Created by z01 on 1/16/2015.
+ */
+public class Globals {
+    public static EditText address;
+}
