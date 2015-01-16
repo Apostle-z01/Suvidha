0b1b0606fe89911b0c76fe3a0414ac131f2e17b4
Manav Kedia <manavkedia1993@gmail.com>
2015-01-17 00:32:58 +0530

    Suvidha Doctor

--- app/src/main/java/com/example/prithwishmukherjee/duvidha/SuvidhaDoctor.java
+++ app/src/main/java/com/example/prithwishmukherjee/duvidha/SuvidhaDoctor.java
@@ -21,6 +21,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.ibm.mobile.services.data.IBMDataException;
+import com.ibm.mobile.services.data.IBMDataObject;
 import com.ibm.mobile.services.data.IBMQuery;
 
 import org.w3c.dom.Text;
@@ -54,6 +55,7 @@ public class SuvidhaDoctor extends ActionBarActivity {
         int rating = 4;
         rb.setRating(rating);
         rb.setIsIndicator(true);
+        formTable();
     }
 
 
@@ -133,7 +135,6 @@ public class SuvidhaDoctor extends ActionBarActivity {
         TextView b1= (TextView)findViewById(R.id.done);
         b1.setVisibility(View.GONE);
 
-
         TableLayout ll = (TableLayout)findViewById(R.id.docAppointmentTable);
         ll.setVisibility(View.VISIBLE);
 
@@ -184,10 +185,13 @@ public class SuvidhaDoctor extends ActionBarActivity {
         Spinner sp1 = (Spinner)findViewById(R.id.dayspinner);
         String appDay = sp1.getSelectedItem().toString();
         Spinner time_hour = (Spinner)findViewById(R.id.doctime_hour);
+        String sp_hour=time_hour.getSelectedItem().toString();
         time_hour.setVisibility(View.GONE);
         Spinner time_min = (Spinner)findViewById(R.id.doctime_min);
+        String sp_min=time_min.getSelectedItem().toString();
         time_min.setVisibility(View.GONE);
         sp1.setVisibility(View.GONE);
+        String appTime=sp_hour+":"+sp_min;
 
         ImageButton tick = (ImageButton)findViewById(R.id.addb);
         tick.setVisibility(View.GONE);
@@ -207,6 +211,30 @@ public class SuvidhaDoctor extends ActionBarActivity {
         ll.setVisibility(View.VISIBLE);
 
         //Add to database using appDay,appTime
+        Doctor_Time doc_time = new Doctor_Time();
+        doc_time.setDay(appDay);
+        doc_time.setTime(appTime);
+        doc_time.setUsername(username);
+        doc_time.save().continueWith(new Continuation<IBMDataObject, Void>() {
+
+            @Override
+            public Void then(Task<IBMDataObject> task) throws Exception {
+                if (task.isFaulted()) {
+                    // Handle errors
+                    Log.e(CLASS_NAME,"Exception: "+task.getError().getMessage());
+                    return null;
+                } else {
+                    Doctor_Time myDoc = (Doctor_Time) task.getResult();
+                    Log.e(CLASS_NAME,myDoc.getTime());
+                    Log.e(CLASS_NAME,myDoc.getDay());
+                    Log.e(CLASS_NAME,myDoc.getUsername());
+
+                    // Do more work
+                }
+                return null;
+            }
+        });
+        formTable();
     }
 
     public void updateDetails(View view)
@@ -242,4 +270,96 @@ public class SuvidhaDoctor extends ActionBarActivity {
         //        intent.putExtra(EXTRA_MESSAGE,username);
         //        startActivity(intent);
     }
+
+    public void formTable(){
+        final TableLayout ll = (TableLayout)findViewById(R.id.docAppointmentTable);
+        ll.setStretchAllColumns(true);
+        ll.setVerticalScrollBarEnabled(true);
+        ll.removeAllViews();
+
+        final Context context = this;
+        try {
+            //To retrieve from the database
+            IBMQuery<Doctor_Time> query = IBMQuery.queryForClass(Doctor_Time.class);
+
+            query.find().continueWith(new Continuation<List<Doctor_Time>, Void>() {
+
+                @Override
+                public Void then(Task<List<Doctor_Time>> task) throws Exception {
+                    int index=0;
+                    if (task.isFaulted()) {
+                        // Handle errors
+                    } else {
+                        // do more work
+                        List<Doctor_Time> objects = task.getResult();
+                        for(final Doctor_Time doc_time:objects){
+                            Log.e(CLASS_NAME, doc_time.getTime());
+                            Log.e(CLASS_NAME, doc_time.getDay());
+
+                            TableRow row = new TableRow(context);
+                            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+                            row.setLayoutParams(lp);
+
+                            //Get from Database
+                            TextView adate = new TextView(context);
+                            adate.setText(doc_time.getDay());
+
+                            TextView atime = new TextView(context);
+                            atime.setText(doc_time.getTime());
+
+                            row.setBackgroundColor(Color.WHITE);
+
+                            ImageButton cancelAppointment = new ImageButton(context);
+                            cancelAppointment.setImageResource(R.drawable.minus);
+                            cancelAppointment.setClickable(true);
+                            cancelAppointment.setBackgroundColor(Color.WHITE);
+                            cancelAppointment.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    // row is your row, the parent of the clicked button
+                                    View row = (View) v.getParent();
+                                    // container contains all the rows, you could keep a variable somewhere else to the container which you can refer to here
+                                    ViewGroup container = ((ViewGroup)row.getParent());
+                                    // delete the row and invalidate your view so it gets redrawn
+                                    TextView temptextview= (TextView) ((ViewGroup)row).getChildAt(0);
+                                    String username = (String) temptextview.getText();
+
+                                    //Delete from Database
+                                    doc_time.delete().continueWith(new Continuation<IBMDataObject, Object>() {
+                                        @Override
+                                        public Object then(Task<IBMDataObject> task) throws Exception {
+                                        if (task.isFaulted()) {
+                                            // Handle errors
+                                            Log.e(CLASS_NAME,"Exception: "+task.getError().getMessage());
+                                            return null;
+                                        } else {
+                                            Doctor_Time myApp = (Doctor_Time) task.getResult();
+                                            Log.e(CLASS_NAME,myApp.getTime());
+                                            Log.e(CLASS_NAME,myApp.getDay());
+
+                                        }
+                                        return null;
+                                        }
+                                    });
+
+                                    container.removeView(row);
+                                    container.invalidate();
+                                }
+                            });
+                            row.addView(adate);
+                            row.addView(atime);
+                            row.addView(cancelAppointment);
+
+                            ll.addView(row, index);
+                            index++;
+                        }
+                    }
+                    return null;
+                }
+            }, Task.UI_THREAD_EXECUTOR);
+        } catch (IBMDataException error) {
+            Log.e(CLASS_NAME,"Exception : " +error.getMessage());
+        }
+    }
+
 }
